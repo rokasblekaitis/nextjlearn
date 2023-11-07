4c646ca202de74fbe57936630ec971e8f78e9600
Rokas Blekaitis <rokas.blekaitis@surfsharkteam.com>
2023-11-07 23:03:56 +0200

    remove console.log

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -23,8 +23,6 @@ export default function EditInvoiceForm({
   const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
   const [state, dispatch] = useFormState(updateInvoiceWithId, initialState);
 
-  console.log(state);
-
   return (
     <form action={dispatch}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
