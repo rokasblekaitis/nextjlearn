bf958dc7eacbde29239c48a42dfb26159e95bce2
Rokas Blekaitis <rokas.blekaitis@surfsharkteam.com>
2023-11-07 17:54:56 +0200

    Chapter 13 - done (error.tsx & not-found.tsx)

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -68,8 +68,6 @@ export async function updateInvoice(id: string, formData: FormData) {
 }
 
 export async function deleteInvoice(id: string) {
-  throw new Error("Failed to Delete Invoice");
-
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
   } catch (error) {
